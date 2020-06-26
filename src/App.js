3d883c6187d1d04168051a0503c163a69973ae7f
Peter Huang <peterhonline@gmail.com>
2020-06-26 13:49:24 -0400

    Redoing operations logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,323 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CAL_STATES = {
+  DEFAULT: {
+    screen: "",
+    display: "0",
+    prevInput: null,
+    answer: 0,
+  },
+  NAN: {
+    screen: "NaN",
+    display: "NaN",
+  },
+
+  OVER_CHAR_LIMIT: {
+    display: "NUM LIMIT REACHED",
+  },
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class="container h-100">
+        <div class="row h-100">
+          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 d-flex flex-column justify-content-center align-items-center">
+            <Calculator />
+
+            <div class="text-center font-weight-bold text-black mt-2">
+              Designed and coded by{" "}
+              <a class="credits" href="https://github.com/peter-huang">
+                Peter Huang
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = CAL_STATES.DEFAULT;
+
+    this.calculate = this.calculate.bind(this);
+    this.isValidNumber = this.isValidNumber.bind(this);
+    this.updateScreen = this.updateScreen.bind(this);
+  }
+
+  updateScreen(state, input) {}
+
+  isValidNumber(state, input) {}
+
+  calculate(event) {
+    let e = event.target.value;
+
+    if (e === "AC") {
+      this.setState(CAL_STATES.DEFAULT);
+    } else if (e === "+" || e === "-" || e === "*" || e === "/") {
+    } else if (e === "=") {
+    } else {
+      // handles numbers and decimals only
+
+      this.setState((state) => ({
+        display: this.isValidNumber(state, e),
+        prevInput: e,
+      }));
+    }
+  }
+
+  render() {
+    return (
+      <div id="calculator">
+        {/* Screen */}
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div id="screen" class="text-right">
+              {this.state.screen}
+            </div>
+          </div>
+        </div>
+
+        {/* Display */}
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div id="display" class="text-right">
+              {this.state.display}
+            </div>
+          </div>
+        </div>
+
+        {/* Controls */}
+        <div class="row" id="controls">
+          <div class="d-none col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="left_bracket"
+              onClick={this.calculate}
+              class="button text-center w-100"
+              value="("
+            >
+              (
+            </button>
+          </div>
+          <div class="d-none col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="right_bracket"
+              onClick={this.calculate}
+              class="button text-center w-100"
+              value=")"
+            >
+              )
+            </button>
+          </div>
+          <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <button
+              id="clear"
+              onClick={this.calculate}
+              class="button text-center w-100"
+              value="AC"
+            >
+              AC
+            </button>
+          </div>
+        </div>
+
+        <div class="row" id="controls">
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="seven"
+              onClick={this.calculate}
+              class="button text-center"
+              value="7"
+            >
+              7
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="eight"
+              onClick={this.calculate}
+              class="button text-center"
+              value="8"
+            >
+              8
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="nine"
+              onClick={this.calculate}
+              class="button text-center"
+              value="9"
+            >
+              9
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="divide"
+              onClick={this.calculate}
+              class="button text-center"
+              value="/"
+            >
+              ÷
+            </button>
+          </div>
+        </div>
+
+        <div class="row" id="controls">
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="four"
+              onClick={this.calculate}
+              class="button text-center"
+              value="4"
+            >
+              4
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="five"
+              onClick={this.calculate}
+              class="button text-center"
+              value="5"
+            >
+              5
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="six"
+              onClick={this.calculate}
+              class="button text-center"
+              value="6"
+            >
+              6
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="multiply"
+              onClick={this.calculate}
+              class="button text-center"
+              value="*"
+            >
+              ×
+            </button>
+          </div>
+        </div>
+
+        <div class="row" id="controls">
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="one"
+              onClick={this.calculate}
+              class="button text-center"
+              value="1"
+            >
+              1
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="two"
+              onClick={this.calculate}
+              class="button text-center"
+              value="2"
+            >
+              2
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="three"
+              onClick={this.calculate}
+              class="button text-center"
+              value="3"
+            >
+              3
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="subtract"
+              onClick={this.calculate}
+              class="button text-center"
+              value="-"
+            >
+              -
+            </button>
+          </div>
+        </div>
+
+        <div class="row" id="controls">
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="zero"
+              onClick={this.calculate}
+              class="button text-center"
+              value="0"
+            >
+              0
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="decimal"
+              onClick={this.calculate}
+              class="button text-center"
+              value="."
+            >
+              .
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="equals"
+              onClick={this.calculate}
+              class="button text-center"
+              value="="
+            >
+              =
+            </button>
+          </div>
+
+          <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+            <button
+              id="add"
+              onClick={this.calculate}
+              class="button text-center"
+              value="+"
+            >
+              +
+            </button>
+          </div>
+        </div>
+
+        {/* end of calculator */}
+      </div>
+    );
+  }
 }
 
 export default App;
